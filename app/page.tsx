5d9212f6021e22905413c8a63d951ec90c89a588
Paul <mierlo.paul@hotmail.nl>
2024-10-20 08:09:42 -0700

    included push payments

--- app/page.tsx
+++ app/page.tsx
@@ -1,40 +1,29 @@
 "use client"
 
-import { useState, useEffect } from "react"
+import { useEffect } from "react"
 import { Wrapper } from "@/components/Wrapper"
 import { useDynamicContext } from "@dynamic-labs/sdk-react-core"
 import { Greeting } from "@/components/Greeting"
-import { useTokenBalances } from "@dynamic-labs/sdk-react-core"
+// import { useTokenBalances } from "@dynamic-labs/sdk-react-core"
 import { useRpcProviders } from "@dynamic-labs/sdk-react-core"
 import { evmProvidersSelector } from "@dynamic-labs/ethereum-core"
-import { useUserWallets } from "@dynamic-labs/sdk-react-core"
+import { useIsLoggedIn } from "@dynamic-labs/sdk-react-core"
+import { useRouter } from "next/navigation"
 
 const Page = () => {
   const { defaultProvider } = useRpcProviders(evmProvidersSelector)
-  const userWallets = useUserWallets()
 
-  const { setShowAuthFlow } = useDynamicContext()
+  const isLoggedIn = useIsLoggedIn()
+  const router = useRouter()
+  // const { setShowAuthFlow } = useDynamicContext()
 
-  const { tokenBalances, isLoading, isError, error } = useTokenBalances()
+  // const { tokenBalances, isLoading, isError, error } = useTokenBalances()
 
   useEffect(() => {
-    userWallets.forEach(async (wallet) => {
-      if (!wallet) return
-
-      // Get the EVM Mainnet provider
-      const provider = defaultProvider?.provider
-
-      if (!provider) return
-
-      // Fetch the wallet balance
-      const balance = await provider.getBalance({
-        address: wallet.address as "0x${string}",
-      })
-
-      console.log("balance", balance.toString())
-      console.log("wallet", wallet.address)
-    })
-  }, [userWallets, defaultProvider])
+    if (isLoggedIn) {
+      router.push("/payments")
+    }
+  }, [isLoggedIn])
 
   return (
     <main>
